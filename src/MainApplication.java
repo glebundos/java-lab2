ebe875475a1eae4bd197f2c180466755cdc81967
lzy77_ <channelforletsplayshik@gmail.com>
2021-11-28 23:07:21 +0300

    ADD MainApplication class, Main_Frame class

---
+++ src/MainApplication.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class MainApplication
+{
+    public static void main(String[]args)
+    {
+        Main_Frame frame = new Main_Frame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+}
---
+++ src/Main_Frame.java
@@ -0,0 +1,6 @@
+import javax.swing.*;
+
+public class Main_Frame extends JFrame
+{
+
+}
